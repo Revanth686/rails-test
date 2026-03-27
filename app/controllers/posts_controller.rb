53d203e18f1993526f7d756d7021d9e514be11cf
revanth-motive <revanth.perumalla@gomotive.com>
2026-03-27 13:33:16 +0530

    tags feature

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,6 +5,7 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
     @posts = @posts.search(params[:q]) if params[:q].present?
+    @posts = @posts.with_tag(params[:tag]) if params[:tag].present?
   end
 
   # GET /posts/1 or /posts/1.json
@@ -68,6 +69,6 @@ class PostsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def post_params
-      params.expect(post: [ :title, :body, :published ])
+      params.expect(post: [ :title, :body, :published, :tag_list ])
     end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -6,4 +6,13 @@ class Post < ApplicationRecord
 
   scope :published, -> { where(published: true) }
   scope :search, ->(query) { where("title LIKE ? OR body LIKE ?", "%#{sanitize_sql_like(query)}%", "%#{sanitize_sql_like(query)}%") }
+  scope :with_tag, ->(tag) { where("tags LIKE ?", "%#{sanitize_sql_like(tag)}%") }
+
+  def tag_list
+    tags.to_s.split(",").map(&:strip).reject(&:empty?)
+  end
+
+  def tag_list=(value)
+    self.tags = value.to_s.split(",").map(&:strip).reject(&:empty?).join(", ")
+  end
 end
